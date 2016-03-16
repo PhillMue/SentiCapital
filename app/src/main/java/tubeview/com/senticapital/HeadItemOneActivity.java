b32929490c3fe7dfafc3824988f2d539d4bcf000
PhillMue <muenzuve@gmail.com>
2016-03-16 06:37:57 +0300

    created head item class

--- app/src/main/java/tubeview/com/senticapital/HeadItemOneActivity.java
+++ app/src/main/java/tubeview/com/senticapital/HeadItemOneActivity.java
@@ -1,7 +1,71 @@
 package tubeview.com.senticapital;
 
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+
+import com.takeoffandroid.appintroanimation.R;
+
+import de.madcyph3r.materialnavigationdrawer.MaterialNavigationDrawer;
+import de.madcyph3r.materialnavigationdrawer.activity.MaterialNavHeadItemActivity;
+import de.madcyph3r.materialnavigationdrawer.head.MaterialHeadItem;
+import de.madcyph3r.materialnavigationdrawer.menu.MaterialMenu;
+import de.madcyph3r.materialnavigationdrawer.menu.item.section.MaterialItemSectionFragment;
+import de.madcyph3r.materialnavigationdrawer.tools.RoundedCornersDrawable;
+
 /**
  * Created by mue on 3/14/16.
  */
-public class HeadItemOneActivity {
+public class HeadItemOneActivity extends MaterialNavHeadItemActivity {
+
+    MaterialNavigationDrawer drawer = null;
+
+    @Override
+    protected boolean finishActivityOnNewIntent() {
+        return false;
+    }
+
+    @Override
+    protected int getNewIntentRequestCode(Class clazz) {
+        return 0;
+    }
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+        drawer = this;
+
+        // information text for the fragment
+        Bundle bundle = new Bundle();
+        bundle.putString("instruction", "This example shows the head item style.");
+
+        FragmentInstruction fragmentInstruction = new FragmentInstruction();
+        fragmentInstruction.setArguments(bundle);
+
+        // create menu
+        MaterialMenu menu = new MaterialMenu();
+        menu.add(new MaterialItemSectionFragment(this, "Instruction", fragmentInstruction, "Head Item Style (One Item)"));
+        menu.add(new MaterialItemSectionFragment(this, "Section 1", new FragmentDummy(), "Section 1"));
+        menu.add(new MaterialItemSectionFragment(this, "Section 2", new FragmentDummy(), "Section 2"));
+        menu.add(new MaterialItemSectionFragment(this, "Section 3", new FragmentDummy(), "Section 3"));
+
+        // create Head Item
+        // use bitmap and make a circle photo
+        final Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.yourAvatar);
+        final RoundedCornersDrawable drawableAppIcon = new RoundedCornersDrawable(getResources(), bitmap);
+        MaterialHeadItem headItem = new MaterialHeadItem(this, "My HeadItem", "My Subtitle", drawableAppIcon, R.drawable.yourBackground, menu);
+        this.addHeadItem(headItem);
+
+        // load menu
+        this.loadMenu(getCurrentHeadItem().getMenu());
+
+        // load the first MaterialItemSectionFragment from the menu
+        this.loadStartFragmentFromMenu(getCurrentHeadItem().getMenu());
+    }
+
+    @Override
+    public void afterInit(Bundle savedInstanceState) {
+
+    }
 }
