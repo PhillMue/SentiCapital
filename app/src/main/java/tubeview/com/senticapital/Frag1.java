36f7b2121f7625774380a678550f1cf8aa0b7e07
PhillMue <muenzuve@gmail.com>
2016-03-16 06:41:22 +0300

    added frag1

---
+++ app/src/main/java/tubeview/com/senticapital/Frag1.java
@@ -0,0 +1,21 @@
+package tubeview.com.senticapital;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.takeoffandroid.appintroanimation.R;
+
+/**
+ * Created by mue on 3/16/16.
+ */
+public class Frag1 extends android.support.v4.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.f1, container, false);
+    }
+
+}
