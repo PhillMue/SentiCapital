4178e0d72465a9b4d023f87afc87cd49036548ac
PhillMue <muenzuve@gmail.com>
2016-03-16 06:37:17 +0300

    created fragment dummy class

--- app/src/main/java/tubeview/com/senticapital/FragmentDummy.java
+++ app/src/main/java/tubeview/com/senticapital/FragmentDummy.java
@@ -1,7 +1,23 @@
 package tubeview.com.senticapital;
 
-/**
- * Created by mue on 3/14/16.
- */
-public class FragmentDummy {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FragmentDummy extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        TextView text = new TextView(this.getActivity());
+        text.setText("Dummy Fragment");
+        text.setGravity(Gravity.CENTER);
+        return text;
+
+    }
 }
